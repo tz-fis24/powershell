ffc47f4dd7f1123e52dca9343ac4a015d134aeac
tz <zantis@riseup.net>
2016-04-12 18:50:24 +0200

    Changed into a function

--- get-sysinfo.ps1
+++ get-sysinfo.ps1
@@ -1,4 +1,7 @@
 ﻿# Get installed .NET Frameworks
+function Get-DotNetVersion
+{
+Clear-Host
 Write-Host 'Installed .NET Framework Components' -ForegroundColor Yellow
 Get-ChildItem 'HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP' -Recurse |
 Get-ItemProperty -Name Version,Release -ErrorAction 0 |
@@ -6,3 +9,5 @@ Where-Object { $_.PSChildName -match '^(?!S)\p{L}'} |
 Select-Object -Property @{N='.NET Component';E={$_.PSChildName}}, @{N='Version';E={$_.Version}} |
 Sort-Object -Property Version -Descending |
 Format-Table -AutoSize
+}
+Get-DotNetVersion
